3fea7e8e1c8b39b82d7d2a67f915f44747060c97
Lake <contact@lakeys.net>
2020-02-03 21:58:25 -0500

    Create devtools.cs
    Create a pre-configured file for dev tools

---
+++ devtools.cs
@@ -0,0 +1,16 @@
+// This is a pre-made configuration file for Jincux's Development Tools
+// 1. Download this: https://blocklandglass.com/addons/addon.php?id=252
+// 2. Copy this file to config/client/devtools.cs
+
+$DevTools::File0 = "add-ons/gamemode_cityrpg4/server/server.cs";
+$DevTools::File1 = "add-ons/gamemode_cityrpg4/server/bricks.cs";
+$DevTools::File2 = "add-ons/gamemode_cityrpg4/server/commands.cs";
+$DevTools::File3 = "add-ons/gamemode_cityrpg4/server/core.cs";
+$DevTools::File4 = "add-ons/gamemode_cityrpg4/server/init.cs";
+$DevTools::File5 = "add-ons/gamemode_cityrpg4/server/package.cs";
+$DevTools::File6 = "add-ons/gamemode_cityrpg4/server/player.cs";
+$DevTools::File7 = "add-ons/gamemode_cityrpg4/server/prefs.cs";
+$DevTools::File8 = "add-ons/gamemode_cityrpg4/server/saving.cs";
+$DevTools::File9 = "add-ons/gamemode_cityrpg4/server/scriptobject.cs";
+$DevTools::File10 = "add-ons/gamemode_cityrpg4/server/cityModules/lotRegistry.cs";
+$DevTools::Files = 11;
