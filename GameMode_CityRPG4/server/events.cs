462bba43360471d52c53ef66af3e609196f1cb15
Lake <contact@lakeys.net>
2020-02-21 23:43:32 -0500

    Create shops.cs and item sell menu base

--- GameMode_CityRPG4/server/events.cs
+++ GameMode_CityRPG4/server/events.cs
@@ -115,7 +115,12 @@ function fxDTSBrick::sellFood(%brick, %portion, %food, %markup, %client)
 
 function fxDTSBrick::sellItem(%brick, %item, %markup, %client)
 {
-	CityMenu_Placeholder(%client);
+	if(!isObject(%client.player))
+	{
+		return;
+	}
+
+	CityMenu_SellItem(%client, %brick, %item, %markup);
 }
 
 function fxDTSBrick::sellClothes(%brick, %item, %markup, %client)
