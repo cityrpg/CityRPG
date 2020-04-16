2583ae17d5ed4b03568bde3585d51498b253fe33
Lake <contact@lakeys.net>
2020-04-15 22:16:20 -0400

    Create the entrepreneur job, balance grocer pay

--- GameMode_CityRPG4/server/jobs/entrepreneur.cs
+++ GameMode_CityRPG4/server/jobs/entrepreneur.cs
@@ -1,14 +1,14 @@
-$CityRPG::jobs::name = "Grocer";
+$CityRPG::jobs::name = "Entrepreneur";
 $CityRPG::jobs::track = "Business";
 $CityRPG::jobs::initialInvestment = 25;
-$CityRPG::jobs::pay = 15;
+$CityRPG::jobs::pay = 30;
 $CityRPG::jobs::tools = "";
 $CityRPG::jobs::datablock = Player9SlotPlayer;
 $CityRPG::jobs::education = 1;
 
 $CityRPG::jobs::sellItems = false;
-$CityRPG::jobs::sellFood = true;
-$CityRPG::jobs::sellItems = false;
+$CityRPG::jobs::sellFood = false;
+$CityRPG::jobs::sellItems = true;
 
 $CityRPG::jobs::law = false;
 $CityRPG::jobs::canPardon = false;
@@ -22,6 +22,7 @@ $CityRPG::jobs::claimer = false;
 $CityRPG::jobs::labor = false;
 
 $CityRPG::jobs::tmHexColor = "00FF00";
-$CityRPG::jobs::helpline = "\c6Grocers can open stores and sell food for profit.";
+$CityRPG::jobs::helpline = "\c6An entrepreneur can run a store and sell general merchandise, but not food or weapons. Their business endeavors net them a slightly higher base pay.";
+$CityRPG::jobs::flavortext = "The best chocolate salesmen in town.";
 
 $CityRPG::jobs::outfit = "none none none none greenShirt blackShirt skin blackPants blackShoes default worm-sweater";
