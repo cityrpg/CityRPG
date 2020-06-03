3226a75e5eb2b708fbe524f3d1d5f991be191137
Lake <contact@lakeys.net>
2020-06-02 21:50:43 -0400

    Create sample ore drop item

--- GameMode_CityRPG4/server/cityModules/laborMining.cs
+++ GameMode_CityRPG4/server/cityModules/laborMining.cs
@@ -30,11 +30,75 @@ datablock fxDTSBrickData(CityRPGSmallOreData)
 };
 
 // ============================================================
+// Mining Resource Drop Datablocks
+// ============================================================
+datablock ShapeBaseImageData(cityOreImage)
+{
+	shapeFile = "base/data/shapes/snowBall.dts";
+	emap = true;
+
+	doColorShift = true;
+	colorShiftColor = "0.2 0.2 0.2 1";
+	canPickup = false;
+};
+
+datablock ItemData(cityOreItem)
+{
+	category = "Weapon";
+	className = "Weapon";
+  uiName = "Ore";
+
+	shapeFile = "base/data/shapes/snowBall.dts";
+	mass = 1;
+	density = 0.2;
+	elasticity = 0.2;
+	friction = 0.6;
+	emap = true;
+
+	doColorShift = true;
+	colorShiftColor = "0.2 0.2 0.2 1";
+	image = cityOreImage;
+	candrop = true;
+	canPickup = false;
+};
+
+// ============================================================
 // Brick Functions
 // ============================================================
 function fxDTSBrick::onCityMine(%this, %client)
 {
 	%client.onCityMine(%this, "Tin");
+
+	// TODO: Use a set amount of damage instead of a random check
+	if(getRandom(1,8) == 1)
+	{
+		%this.cityOreBreak(%client);
+	}
+}
+
+function fxDTSBrick::citySpawnOre(%this)
+{
+	%item = new Item()
+	{
+		dataBlock = cityOreItem;
+		static = 0;
+	};
+
+	%item.setVelocity(getRandom(0,2) SPC getRandom(0,2) SPC getRandom(0,2));
+	%item.setTransform(%this.getPosition() SPC "0 0 0 0");
+	%item.schedulePop(); // TODO: Extend the de-spawn time
+	%item.spawnBrick = %this;
+}
+
+function fxDTSBrick::cityOreBreak(%this, %client)
+{
+	for(%i = 0; %i <= getRandom(2,4); %i++)
+	{
+		%this.citySpawnOre();
+	}
+
+	%this.disappear(getRandom(30,60));
+	%this.playSound(brickBreakSound);
 }
 
 // ============================================================
