7629e9a6cdbc96b24fc1fb7723731d4f7fe04cac
Lake <contact@lakeys.net>
2020-07-02 16:45:35 -0400

    Refactor demerit cost referencing
    Refactor and optimize the way demerit costs are written and referenced by moving assignment to a new function, refreshCityDemeritCosts, and assigning them to the client. Consolidate the horrific overuse of CityRPGData.getData to a single call, %data.

--- GameMode_CityRPG4/server/brickScripts/info/police.cs
+++ GameMode_CityRPG4/server/brickScripts/info/police.cs
@@ -19,27 +19,30 @@ datablock fxDTSBrickData(CityRPGPoliceBrickData : brick2x4FData)
 // ============================================================
 // Trigger Data
 // ============================================================
+function GameConnection::refreshCityDemeritCosts(%client)
+{
+	%data = CityRPGData.getData(%client.bl_id);
+
+	%client.dems = %data.valueDemerits;
+	%client.demsAffordableTotal = mFloor(%data.valueMoney / $Pref::Server::City::demerits::demeritCost);
+	%client.demsAffordable = (%client.demsAffordableTotal > %client.dems ? %client.dems : %client.demsAffordableTotal);
+	%client.demCost = mFloor(%client.demsAffordable * $Pref::Server::City::demerits::demeritCost);
+}
+
 function CityRPGPoliceBrickData::parseData(%this, %brick, %client, %triggerStatus, %text)
 {
 	if(%triggerStatus !$= "")
 	{
 		if(%triggerStatus == true)
 		{
-			if(CityRPGData.getData(%client.bl_id).valueDemerits)
-			{
-				%yourDemerits = CityRPGData.getData(%client.bl_id).valueDemerits;
-				%totalPrice = mFloor(CityRPGData.getData(%client.bl_id).valueDemerits * $Pref::Server::City::demerits::demeritCost);
-				%demsYouCanAfford = mFloor(CityRPGData.getData(%client.bl_id).valueMoney / $Pref::Server::City::demerits::demeritCost);
-				%demsYouCanBuy = (%demsYouCanAfford > %yourDemerits ? %yourDemerits : %demsYouCanAfford);
-				%demCost = mFloor(%demsYouCanBuy * $Pref::Server::City::demerits::demeritCost);
-			}
+			%client.refreshCityDemeritCosts();
 
 			messageClient(%client, '', "\c3" @ $Pref::Server::City::name @ " Police Department");
 			messageClient(%client, '', "\c6Type a number in chat:");
 
 			if(CityRPGData.getData(%client.bl_id).valueDemerits > 0)
 			{
-				messageClient(%client, '', "\c6You have \c3" @ CityRPGData.getData(%client.bl_id).valueDemerits SPC "\c6demerits.");
+				messageClient(%client, '', "\c6You have \c3" @ %dems SPC "\c6demerits.");
 			}
 
 			messageClient(%client, '', "\c31 \c6- View Online Criminals");
@@ -47,16 +50,15 @@ function CityRPGPoliceBrickData::parseData(%this, %brick, %client, %triggerStatu
 			%cost = %client.getCityRecordClearCost();
 			messageClient(%client, '', "\c32 \c6- Clear your record! (\c3$" @ %cost @ "\c6)");
 
-
 			if(CityRPGData.getData(%client.bl_id).valueDemerits)
 			{
 				if(%demsYouCanBuy >= %yourDemerits)
 				{
-					messageClient(%client, '', "\c33 \c6- Pay off Demerits (\c3$" @ %demCost @ "\c6)");
+					messageClient(%client, '', "\c33 \c6- Pay off Demerits (\c3$" @ %client.demCost @ "\c6)");
 				}
 				else
 				{
-					messageClient(%client, '', "\c33 \c6- Pay Partial Demerits (\c3" @ %demsYouCanBuy @ "\c6 out of \c3" @ %yourDemerits @ "\c6 for \c3$" @ %demCost @ "\c6)");
+					messageClient(%client, '', "\c33 \c6- Pay Partial Demerits (\c3" @ %client.demsAffordable @ "\c6 out of \c3" @ %client.dems @ "\c6 for \c3$" @ %client.demCost @ "\c6)");
 				}
 			}
 			if(CityRPGData.getData(%client.bl_id).valueevidence)
@@ -116,13 +118,9 @@ function CityRPGPoliceBrickData::parseData(%this, %brick, %client, %triggerStatu
 
 	if((strReplace(%input, "3", "") !$= %input || strReplace(%input, "three", "") !$= %input) && CityRPGData.getData(%client.bl_id).valueDemerits > 0)
 	{
-		%yourDemerits = CityRPGData.getData(%client.bl_id).valueDemerits;
-		%totalPrice = mFloor(CityRPGData.getData(%client.bl_id).valueDemerits * $Pref::Server::City::demerits::demeritCost);
-		%demsYouCanAfford = mFloor(CityRPGData.getData(%client.bl_id).valueMoney / $Pref::Server::City::demerits::demeritCost);
-		%demsYouCanBuy = (%demsYouCanAfford > %yourDemerits ? %yourDemerits : %demsYouCanAfford);
-		%demCost = mFloor(%demsYouCanBuy * $Pref::Server::City::demerits::demeritCost);
+		%client.refreshCityDemeritCosts();
 
-		if(%demsYouCanBuy <= 0)
+		if(%client.demsAffordable <= 0)
 		{
 			messageClient(%client, '', "\c6You cant afford to pay off any demerits!");
 
@@ -138,10 +136,10 @@ function CityRPGPoliceBrickData::parseData(%this, %brick, %client, %triggerStatu
 			return;
 		}
 
-		CityRPGData.getData(%client.bl_id).valueMoney -= %demCost;
-		CityRPGData.getData(%client.bl_id).valueDemerits -= %demsYouCanBuy;
+		CityRPGData.getData(%client.bl_id).valueMoney -= %client.demCost;
+		CityRPGData.getData(%client.bl_id).valueDemerits -= %client.demsAffordable;
 
-		messageClient(%client, '', "\c6You have paid \c3$" @ %demCost @ "\c6. You now have\c3" SPC (CityRPGData.getData(%client.bl_id).valueDemerits ? CityRPGData.getData(%client.bl_id).valueDemerits : "no") SPC "\c6demerits.");
+		messageClient(%client, '', "\c6You have paid \c3$" @ %client.demCost @ "\c6. You now have\c3" SPC (CityRPGData.getData(%client.bl_id).valueDemerits ? CityRPGData.getData(%client.bl_id).valueDemerits : "no") SPC "\c6demerits.");
 
 		%client.setInfo();
 
