4fcbf21db9c9dcd5e52409f69433e4b34b54a7ff
Lake <contact@lakeys.net>
2020-08-12 19:10:47 -0400

    Implement options to list a lot for sale

--- GameMode_CityRPG4/server/brickScripts/info/realestate.cs
+++ GameMode_CityRPG4/server/brickScripts/info/realestate.cs
@@ -41,8 +41,7 @@ function CityMenu_RealEstate(%client, %brick)
 			%message = %message SPC "\c6There are \c3" @ $City::RealEstate::LotCountSale @ "\c6 pre-owned lots for sale.";
 
 		%menu = "List a lot for sale"
-				TAB "View pre-owned lots for sale"
-				TAB "Purchase a pre-owned lot";
+				TAB "View pre-owned lots for sale";
 
 		%functions = "CityMenu_RealEstate_ListForSalePrompt"
 						 TAB "CityMenu_Placeholder"
@@ -74,12 +73,12 @@ function CityMenu_RealEstate_ListForSalePrompt(%client, %input, %brick)
 		if(%i == 0)
 		{
 			%menu = %option;
-			%functions = CityMenu_RealEstate_ListForSale;
+			%functions = CityMenu_RealEstate_ListForSalePricePrompt;
 		}
 		else
 		{
 			%menu = %menu TAB %option;
-			%functions = %functions TAB CityMenu_RealEstate_ListForSale;
+			%functions = %functions TAB CityMenu_RealEstate_ListForSalePricePrompt;
 		}
 	}
 
@@ -87,13 +86,66 @@ function CityMenu_RealEstate_ListForSalePrompt(%client, %input, %brick)
 	%client.cityMenuMessage("\c6Choose one of your lots to list for sale. Use the PG UP and PG DOWN keys to scroll.");
 }
 
-function CityMenu_RealEstate_ListForSale(%client, %input)
+function CityMenu_RealEstate_ListForSalePricePrompt(%client, %input)
 {
 	%i = atof(%input)-1;
 	%lotBrick = getWord($City::Cache::LotsOwnedBy[%client.bl_id], %i);
+	%client.cityLotSelected = %lotBrick;
 
-	%client.cityMenuMessage("\c6You have chosen the lot: " @ %lotBrick.getCityLotName());
-	CityMenu_Placeholder(%client);
+	%client.cityMenuMessage("\c6You have chosen the lot: \c3" @ %lotBrick.getCityLotName());
+	%client.cityMenuMessage("\c6How much money would you like to sell this lot for? Enter a number.");
+
+	%client.cityMenuFunction = CityMenu_RealEstate_ListForSaleConfirmPrompt;
+}
+
+function CityMenu_RealEstate_ListForSaleConfirmPrompt(%client, %input)
+{
+	%price = atof(%input);
+	%lotBrick = %client.cityLotSelected;
+
+	if(%price < 0)
+		%price = 0;
+
+	%client.cityMenuMessage("\c6You are listing the lot \c3" @ %lotBrick.getCityLotName() @ "\c6 on sale for \c3$" @ strFormatNumber(%price));
+	%client.cityMenuMessage("\c0Warning!\c6 Once a player purchases this lot, they will become the permanent owner of your lot. Are you sure?");
+
+	if(%price == 0)
+		%client.cityMenuMessage("\c0You are about to list this lot for free. Are you sure?");
+
+	%client.cityMenuMessage("\c6Type \c31\c6 to confirm, or \c32\c6 to cancel.");
+
+	%client.cityMenuFunction = CityMenu_RealEstate_ListForSale;
+}
+
+function CityMenu_RealEstate_ListForSale(%client, %input)
+{
+	%lotBrick = %client.cityLotSelected;
+
+	if(%input !$= "1")
+	{
+		%client.cityMenuMessage("\c0Lot listing cancelled.");
+		%client.cityMenuClose();
+		return;
+	}
+
+	// Security check
+	if(%lotBrick.getCityLotOwnerID() != %client.bl_id)
+	{
+		%client.cityLog("(!!!) Lot " @ %lotBrick.getCityLotID() @ " sale listing fell through");
+
+		// Security check falls through
+		%client.cityMenuMessage("\c0Sorry, you are no-longer able to list that lot for sale at this time.");
+		%client.cityMenuClose();
+		return;
+	}
+
+	$City::RealEstate::LotCountSale++;
+
+	// Append the lot to the fields under CitySO.lotListings.
+	CitySO.lotListings = CitySO.lotListings $= ""? CitySO.lotListings = %lotBrick : CitySO.lotListings = CitySO.lotListings TAB %lotBrick;
+
+	%client.cityMenuMessage("\c6You have listed your lot for sale.");
+	%client.cityMenuClose();
 }
 
 // ============================================================
