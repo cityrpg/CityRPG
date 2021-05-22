7ea82e53160c9fc868a642337cbc875199447ef1
Lake <contact@lakeys.net>
2021-05-22 19:17:31 -0400

    Update cityMenuOpen comment

--- GameMode_CityRPG4/server/core.cs
+++ GameMode_CityRPG4/server/core.cs
@@ -4,7 +4,7 @@
 
 // Client.cityMenuOpen(names, functions, exitMsg, autoClose, canOverride)
 // Modular function hook for displaying menus in-game.
-// Currently utilizes chat just like classic CityRPG, however this is subject to change.
+// Utilizes a combination of Conan's center-print menus for selections, and chat inputs for text.
 function GameConnection::cityMenuOpen(%client, %menu, %functions, %menuID, %exitMsg, %autoClose, %canOverride, %title)
 {
 	if(%client.cityMenuOpen && !%client.cityMenuCanOverride)
