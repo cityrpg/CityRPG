c23f9180f7d03029903297614724b7be1df93ed2
Lake <contact@lakeys.net>
2022-09-10 18:30:12 -0400

    Update jobs template mod

--- CityRPG_Jobs_Template/server.cs
+++ CityRPG_Jobs_Template/server.cs
@@ -1,8 +1,8 @@
 %error = ForceRequiredAddOn("GameMode_CityRPG4");
 if(%error == $Error::AddOn_NotFound)
 {
-  error("ERROR: Support_CityRPG_JobMod_Template - required add-on GameMode_CityRPG4 not found");
+  error("ERROR: CityRPG_Jobs_Template - required add-on GameMode_CityRPG4 not found");
   return;
 }
 
-exec("./Support_CityRPG_JobMod_Template.cs");
+exec("./CityRPG_Jobs_Template.cs");
