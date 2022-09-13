a23cb10cc17ddfe3d011da353bbd0b07dab359f8
Lake <contact@lakeys.net>
2022-09-12 22:53:03 -0400

    Fix players dropping their money while jailed
    Caused by the saver rework, 80f85fc

--- GameMode_CityRPG4/server/cityModules/cash.cs
+++ GameMode_CityRPG4/server/cityModules/cash.cs
@@ -6,7 +6,7 @@ package CityRPG_Cash
 	// Drop Money
 	function gameConnection::onDeath(%client, %killerPlayer, %killer, %damageType, %unknownA)
 	{
-		if(!getWord(City.get(%client.bl_id).valueJailData, 1) && City.get(%client.bl_id, "money") && !%client.moneyOnSuicide)
+		if(!getWord(City.get(%client.bl_id, "jaildata"), 1) && City.get(%client.bl_id, "money") && !%client.moneyOnSuicide)
 		{
 			if($Pref::Server::City::misc::cashdrop == 1)
 			{
